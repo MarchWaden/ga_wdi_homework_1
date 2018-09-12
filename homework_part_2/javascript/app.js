ed192e4bb5a10e380dbc21d022d849bdea0b94a4
MarchWaden <max.linz@live.com>
2018-09-11 22:08:25 -0600

    Part 1 terminology questions done

---
+++ homework_part_2/javascript/app.js
@@ -0,0 +1,8 @@
+//1. interpolation is wierd and concatenation is straightforward appending strings to each other
+//2. Don't Repeat Yourself. Loops and functions.
+//3. Declaring a variable allocates memory for it and registers it as existing. Assigning a value changes what goes in that memory.
+//4. We should use let when we need to change the value of a variable, and otherwise use const
+//5. A directory containing one or more child directories.
+//6. type -help after the command
+//7. it autocompletes parts of folder or file names, and certain commands in some cases. It lets you type less,
+//which means less time spent and more importantly fewer typos.
