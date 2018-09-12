8781be5c8dd0e4d369611637030198e14b94b727
MarchWaden <max.linz@live.com>
2018-09-12 16:50:07 -0600

    Completed homework 1

--- homework_sections_2_3_4/javascript/app.js
+++ homework_sections_2_3_4/javascript/app.js
@@ -1,4 +1,4 @@
-//1. interpolation is wierd and concatenation is straightforward appending strings to each other
+//1. interpolation does not change the value of either of the things being interpolated, concatenation does?
 //2. Don't Repeat Yourself. Loops and functions.
 //3. Declaring a variable allocates memory for it and registers it as existing. Assigning a value changes what goes in that memory.
 //4. We should use let when we need to change the value of a variable, and otherwise use const
@@ -18,8 +18,22 @@
 
 //While loops questions
 //1. infinite
-//2. code won't execute so it's not infinite
+//2. code won't execute at all so it's not infinite
 //3. code will output a string containing the letter "A" 20 times
 
 //Loops questions, part 4
-//1.
+//1. While loops execute until a condition is met, for loops execute
+//a defined number of times and use an index variable
+//2.
+for (let i = 0; i <1000; i++){
+  console.log(i);
+}
+//3. definition, condition, iterator
+//4.
+for (let i = 999; i>-1; i--){
+  console.log(i);
+}
+//5.
+for (i = 1; 1 <11; 1++){
+  consolge.log("The value of i is: " + i);
+}
